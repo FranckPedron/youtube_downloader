a023bc3f5fa05e5989859b8258276024a533ae92
FranckPedron <franckyp1+rapports_git@gmail.com>
2024-05-10 11:59:25 +0200

    Ajout progression du téléchargement

--- main.py
+++ main.py
@@ -2,7 +2,17 @@ from pytube import YouTube
 
 url = "https://www.youtube.com/watch?v=8VfsciIdJrs"
 
+
+def on_download_progress(stream, chunk, bytes_remaining):
+    bytes_downloaded = stream.filesize - bytes_remaining
+    percent = bytes_downloaded * 100 / stream.filesize
+
+    print(f"Progression du téléchargement {int(percent)}%")
+
+
 youtube_video = YouTube(url)
+youtube_video.register_on_progress_callback(on_download_progress)
+
 print("Titre:", youtube_video.title)
 
 print("STREAMS")
