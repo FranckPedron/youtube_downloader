9d9b6ea58bc2cb00c0659597364894e1e259b9ca
FranckPedron <franckyp1+rapports_git@gmail.com>
2024-05-10 11:07:49 +0200

    Ajout téléchargement plus haute résolution

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 from pytube import YouTube
+
+url = "https://www.youtube.com/watch?v=8VfsciIdJrs"
+
+youtube_video = YouTube(url)
+print("Titre:", youtube_video.title)
+
+print("STREAMS")
+for stream in youtube_video.streams.fmt_streams:
+    print(" ", stream)
+
+stream = youtube_video.streams.get_highest_resolution()
+print("Téléchargement...")
+stream.download()
+print("OK")
