e46d51dd6f3d4d870353b6473367b5a4379ef6cd
FranckPedron <franckyp1+rapports_git@gmail.com>
2024-05-13 16:41:02 +0200

    Ajout du choix de stream à charger

--- main.py
+++ main.py
@@ -23,10 +23,35 @@ youtube_video.register_on_progress_callback(on_download_progress)
 print("Titre:", youtube_video.title)
 
 print("STREAMS")
-for stream in youtube_video.streams.fmt_streams:
-    print(" ", stream)
+all_streams = youtube_video.streams
 
-stream = youtube_video.streams.get_highest_resolution()
+print("Résolutions disponibles: ")
+
+streams = all_streams.filter(progressive=True, file_extension="mp4")
+index = 1
+for stream in streams:
+    print(index, "-", stream.resolution, "-", stream.itag)
+    index += 1
+
+while True:
+    num_res = input("Quelle résolution choisissez-vous ? ")
+    if num_res == "":
+        print("Erreur! Vous devez choisir une résolution.")
+    else:
+        try:
+            int_num_res = int(num_res)
+        except ValueError:
+            print("Erreur! Vous devez entrer un chiffre")
+        else:
+            if not 1 <= int_num_res <= len(streams):
+                print("Erreur! Vous devez entrer un nombre entre 1 et ", len(streams))
+            else:
+                break
+
+itag_index = streams[int_num_res - 1].itag
+
+stream = streams.get_by_itag(itag_index)
+print(stream)
 print("Téléchargement...")
 stream.download()
 print("OK")
